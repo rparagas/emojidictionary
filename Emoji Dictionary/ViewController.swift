24f62033986b70e0af6e4b8b19c21b27f76cf99e
rparagas <rayparagas@rays-air.home>
2016-12-06 13:00:59 +1100

    added second view controller and emoji array

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var emojiDictTableView: UITableView!
     
+    var emojis = ["🐶","💩","💀","🙈","🙉","🙊"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,12 +22,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return emojis.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Alyssa is the best"
+        cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
 
